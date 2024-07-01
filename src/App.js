4146eff75e79b1ca03785e447114ad9882067001
Sarah Felts <sarahfelts44@gmail.com>
2024-07-01 14:02:34 -0500

    App finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
-function App() {
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  const [value, setValue] = useState('0');
+
+  const handleClick = (btnValue) => {
+    setValue((prev) => {
+      if (btnValue === 'C') {
+        return '0';
+      }
+      if (btnValue === '=') {
+        try {
+          return eval(prev).toString();
+        } catch (e) {
+          return 'Error';
+        }
+      }
+      return prev === '0' ? btnValue : prev + btnValue;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value={value} />
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === '=' ? 'equals' : ''}
+                value={btn}
+                onClick={() => handleClick(btn)}
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
+
 
 export default App;
